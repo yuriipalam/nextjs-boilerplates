e483af00f16870d111dd72772cb73a1cc2963415
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-07-28 16:17:44 +0200

    Add link to Examples page

--- src/modules/dashboard.tsx
+++ src/modules/dashboard.tsx
@@ -95,6 +95,11 @@ export function Dashboard() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <Link href="/examples/mail" className="mx-auto mt-5">
+        <Button variant="default" size="lg">
+          SEE MORE EXAMPLES HERE
+        </Button>
+      </Link>
       <TooltipProvider>
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
           <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
